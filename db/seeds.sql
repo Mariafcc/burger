ac960771b26a0e70c964f78c105d99257ed016c8
MariaFernanda Cruz <mariafernandacruz@MariaFernandas-MacBook-Pro.local>
2020-09-26 14:41:15 -0500

    changes to burgers seeds insert

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,5 @@
-INSERT INTO burgers (burger_name)
-VALUES 
-("bacon cheese burgers"), 
-("chicken burgers"),
-("spicy chicken burgers"),
-("vegan burgers")
 
-INSERT INTO burgers (name) VALUES ('bacon cheese burgers');
-INSERT INTO burgers (name) VALUES ('chicken burgers');
-INSERT INTO burgers (name, devoured) VALUES ('spicy chicken burgers', true);
-INSERT INTO burgers (name, devoured) VALUES ('vegan burgers', true);
+INSERT INTO burgers (burger_name) VALUES ('bacon cheese burgers');
+INSERT INTO burgers (burger_name) VALUES ('chicken burgers');
+INSERT INTO burgers (burger_name, devoured) VALUES ('spicy chicken burgers', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('vegan burgers', true);
